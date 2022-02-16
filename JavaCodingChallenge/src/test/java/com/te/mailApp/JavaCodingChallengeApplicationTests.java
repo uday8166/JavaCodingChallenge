80d1d48ef791b1e448a5ebb379bf3e89eaa1b58c
uday8166 <udaybandla81@gmail.com>
2022-02-16 13:48:33 +0530

    Coding Challenge

---
+++ JavaCodingChallenge/src/test/java/com/te/mailApp/JavaCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te.mailApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
